6bc975a320749b9f7e2b1bf6aa879935f96eadcc
Kevin Ditscheid <kevin@the-coding-owl.de>
2021-05-31 23:44:48 +0200

    install webpack and vue

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const Encore = require('@symfony/webpack-encore');
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    .setOutputPath('src/site/public')
+    .setPublicPath('src/site/public')
+    .addEntry('app', 'src/site/assets/app.js')
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+    .enableIntegrityHashes(Encore.isProduction())
+;
+
+module.exports = Encore.getWebpackConfig();
